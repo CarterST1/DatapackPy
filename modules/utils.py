0d720a3019044861768d9fddf6ff39fcf6a095eb
CarterST1 <cobertrand@outlook.com>
2025-08-30 09:39:53 -0500

    Added pack versions and `getPackFormatRange` in `utils`

--- modules/utils.py
+++ modules/utils.py
@@ -1,3 +1,6 @@
+from typing import overload
+
+
 LEGAL_CHARS = '0123456789abcdefghijklmnopqrstuvwxyz_.'
 
 def isLegalChar(char: str):
@@ -43,3 +46,32 @@ def convertFromResourceLocation(location: str):
     resovedPath = splitLocation[1]
 
     return True, resolvedNamespace, resovedPath
+
+PACK_VERSIONS = {
+    '4': {'minimum': (1, 13, 0), 'maximum': (1, 14, 4)},
+    '5': {'minimum': (1, 15, 0), 'maximum': (1, 16, 1)},
+    '6': {'minimum': (1, 16, 2), 'maximum': (1, 16, 5)},
+    '7': {'minimum': (1, 17, 0), 'maximum': (1, 17, 1)},
+    '8': {'minimum': (1, 18, 0), 'maximum': (1, 18, 1)},
+    '9': {'minimum': (1, 18, 2), 'maximum': (1, 18, 2)},
+    '10': {'minimum': (1, 19, 0), 'maximum': (1, 19, 3)},
+    '12': {'minimum': (1, 19, 4), 'maximum': (1, 19, 4)},
+    '15': {'minimum': (1, 20, 0), 'maximum': (1, 20, 1)},
+    '18': {'minimum': (1, 20, 2), 'maximum': (1, 20, 2)},
+    '26': {'minimum': (1, 20, 3), 'maximum': (1, 20, 4)},
+    '41': {'minimum': (1, 20, 5), 'maximum': (1, 20, 6)},
+    '48': {'minimum': (1, 21, 0), 'maximum': (1, 21, 1)},
+    '57': {'minimum': (1, 21, 2), 'maximum': (1, 21, 3)},
+    '61': {'minimum': (1, 21, 4), 'maximum': (1, 21, 4)},
+    '71': {'minimum': (1, 21, 5), 'maximum': (1, 21, 5)},
+    '80': {'minimum': (1, 21, 6), 'maximum': (1, 21, 6)},
+    '81': {'minimum': (1, 21, 7), 'maximum': (1, 21, 8)},
+    '85': {'minimum': (1, 21, 9), 'maximum': (1, 21, 9)},
+}
+
+def getPackFormatRange(formatNumber: int):
+    version = PACK_VERSIONS.get(str(formatNumber))
+    if version:
+        return version.get('minimum', (1, 21, 8)), version.get('minimum', (1, 21, 8))
+    else:
+        return None
