e439a880cac07e4bb8bee406dafde28ef78935ab
CarterST1 <cobertrand@outlook.com>
2025-08-31 09:20:06 -0500

    Added `__repr__` to `function.py`

--- datapackpy/core/function.py
+++ datapackpy/core/function.py
@@ -20,6 +20,20 @@ class Function:
         """Return the function as a string ready to write to a .mcfunction file."""
         return "\n".join(self.commands)
     
+    def __repr__(self) -> str:
+        string = f'<Function \'{self.name}\' | '
+        string += f'{len(self.commands)} commands: '
+        for i, command in enumerate(self.commands[:3]):
+            if len(command) > 50:
+                command = command[:50] + '...'
+            string += f'\'{command}\''
+            if i < len(self.commands[:3]) - 1:  # add comma except after the last command
+                string += ', '
+        if len(self.commands) > 3:
+            string += f', ... (+{len(self.commands) - 3} more)'
+        string += '>'
+        return string
+
     def create_function_file(self, path_dir: Path):
         function_file = Path(path_dir / f'{self.name}.mcfunction')
         function_file.write_text(str(self))
