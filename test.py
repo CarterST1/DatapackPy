71f886088f7197b6eb48fe0ade21365b8bca3722
CarterST1 <cobertrand@outlook.com>
2025-08-31 07:30:44 -0500

    Fixed `test.py`

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
 import datapackpy
 
-print(datapackpy.internal.utils.convertFromResourceLocation('minecraft/villager'))
-
-print(datapackpy.internal.utils.formatWithNamespace('minecraft', 'test'))
-print(datapackpy.internal.utils.formatWithNamespace('Minecraft', 'test'))
-print(datapackpy.internal.utils.getPackFormatRange(80))
-print(datapackpy.internal.GameVersion.fromTuple((1, 19)))
-
-print(datapackpy.internal.utils.getPackFormatId(datapackpy.internal.GameVersion.fromTuple((1, 21, 9))))
-
 dataPack = datapackpy.DataPack((1, 21, 1), 'cartermods', 'cst')
 print(dataPack)
 
