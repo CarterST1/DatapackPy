74d4fe502ff43e66b3983d475a597451be12ac9d
CarterST1 <cobertrand@outlook.com>
2025-08-30 19:59:09 -0500

    Added `Function` to `__all__` and added module description

--- datapackpy/__init__.py
+++ datapackpy/__init__.py
@@ -1,4 +1,7 @@
+"""Library that makes Minecraft datapack creation easier"""
+
 from .datapack import DataPack
 from . import internal
+from . import core
 
-__all__ = ["DataPack", "internal"]  # only show top-level things
+__all__ = ["DataPack", "internal", "core"]  # only show top-level things
