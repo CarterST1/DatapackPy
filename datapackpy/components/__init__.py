e0f6c6d5941a3f601e883e877f92f50ba49304ca
CarterST1 <cobertrand@outlook.com>
2025-09-01 19:22:49 -0500

    Added `Advancement` to component's `__init__.py`

--- datapackpy/components/__init__.py
+++ datapackpy/components/__init__.py
@@ -4,12 +4,14 @@ datapackpy.components
 Core package containing the primary building blocks of Minecraft datapacks.
 
 This package exposes only the public-facing APIs:
+- `Advancement`: represents a Minecraft advancement
 - `Function`: represents a Minecraft function (a sequence of commands).
 - `PackMeta`: contains metadata for teh datapack
-- (Future classes may include `Advancement`, `LootTable`, etc.)
+- (Future classes may include `LootTable`, etc.)
 """
 
+from .advancement import *
 from .function import *
 from .pack_meta import *
 
-__all__ = ['Function', 'PackMeta']
+__all__ = ['Advancement', 'Function', 'PackMeta']
